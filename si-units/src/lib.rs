ffce15a5d97c986d8e1adb85ef90ef6573355e9a
Gernot Bauer <bauer@itt.uni-stuttgart.de>
2021-11-08 11:21:32 +0100

    Changed module wrap to function call

--- si-units/src/lib.rs
+++ si-units/src/lib.rs
@@ -1,9 +1,8 @@
 use pyo3::prelude::*;
-use pyo3::wrap_pymodule;
-use quantity::python::PyInit_quantity;
+use quantity::python::quantity;
 
 /// Implementation of SI numbers.
 #[pymodule]
 pub fn si_units(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
-    m.add_wrapped(wrap_pymodule!(quantity))
+    quantity(_py, m)
 }
