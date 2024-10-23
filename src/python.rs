2664be7e4875ac3fcc90269c90de5b2563c8cd70
Philipp Rehner <prehner@ethz.ch>
2024-10-23 14:37:01 +0200

    also give error message on other types that implement __getnewargs__

--- src/python.rs
+++ src/python.rs
@@ -58,14 +58,16 @@ where
     Self: PrintUnit,
 {
     fn extract_bound(ob: &Bound<'py, PyAny>) -> PyResult<Self> {
-        let Ok(raw) = ob.call_method0("__getnewargs__") else {
+        let Ok((value, unit_from)) = ob
+            .call_method0("__getnewargs__")
+            .and_then(|raw| raw.extract::<(f64, [i8; 7])>())
+        else {
             return Err(PyErr::new::<PyValueError, _>(format!(
                 "Missing units! Expected {}, got {}.",
                 Self::UNIT,
                 ob.call_method0("__repr__")?
             )));
         };
-        let (value, unit_from) = raw.extract::<(f64, [i8; 7])>()?;
         let unit_into = [L::I8, M::I8, T::I8, I::I8, N::I8, THETA::I8, J::I8];
         if unit_into == unit_from {
             Ok(Quantity(value, PhantomData))
@@ -94,14 +96,16 @@ where
     Self: PrintUnit,
 {
     fn extract_bound(ob: &Bound<'py, PyAny>) -> PyResult<Self> {
-        let Ok(raw) = ob.call_method0("__getnewargs__") else {
+        let Ok((value, unit_from)) = ob
+            .call_method0("__getnewargs__")
+            .and_then(|raw| raw.extract::<(PyReadonlyArray<f64, D>, [i8; 7])>())
+        else {
             return Err(PyErr::new::<PyValueError, _>(format!(
                 "Missing units! Expected {}, got {}.",
                 Self::UNIT,
                 ob.call_method0("__repr__")?
             )));
         };
-        let (value, unit_from) = raw.extract::<(PyReadonlyArray<f64, D>, [i8; 7])>()?;
         let value = value.as_array().to_owned();
         let unit_into = [L::I8, M::I8, T::I8, I::I8, N::I8, THETA::I8, J::I8];
         if unit_into == unit_from {
